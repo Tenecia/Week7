8752a0ac91a27cc636c0894c0cc35651ae8c4528
Tenecia Guinea <aja.guinea@gmail.com>
2015-08-14 17:54:38 -0400

    CodeEval

---
+++ codeEvaldecimaltoBinary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEvaldecimalTobinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a Number");
+            int DecimalVal = int.Parse(Console.ReadLine());
+            string BinaryVal = Convert.ToString(DecimalVal, 2); //method
+            Console.WriteLine(BinaryVal);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
